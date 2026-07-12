3929732b8298bcc86bd580528574c11d10b3aeba
HenrBmp <henriquerodr20@gmail.com>
2026-07-12 16:24:06 -0300

    set theme "codedark" in lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,5 +1,9 @@
-vim.pack.add({
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/nvim-lualine/lualine.nvim'
-})
-require("lualine").setup()
+vim.pack.add {
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/nvim-lualine/lualine.nvim',
+}
+require('lualine').setup {
+  options = {
+    theme = 'codedark',
+  },
+}
