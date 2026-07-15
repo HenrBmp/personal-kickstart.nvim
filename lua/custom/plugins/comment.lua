8407f81838caf6e92bd5b1fc30a6f4af8dad2b64
HenrBmp <henriquerodr20@gmail.com>
2026-07-15 10:52:28 -0300

    comment plugin keymap

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,4 +1,11 @@
 local gh = require 'custom.utils.gh'
 
 vim.pack.add { gh 'numToStr/Comment.nvim' }
-require('Comment').setup()
+require('Comment').setup {
+  toggler = {
+    line = '<M-;>',
+  },
+  opleader = {
+    block = '<M-A>',
+  },
+}
